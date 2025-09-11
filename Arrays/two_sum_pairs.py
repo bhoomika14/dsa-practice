4e364fb419f030aeb63230882634ac4b6ba402ec
bhoomika14 <bhoomikaudaykumar99@gmail.com>
2025-09-11 15:23:25 +0530

    Find two sum pairs

--- Arrays/two_sum_pairs.py
+++ Arrays/two_sum_pairs.py
@@ -13,5 +13,36 @@ def two_sum(arr):
                 result.append([arr[i], arr[j]])
         
     return result
+
+# Approach 2
+def two_sum_pairs(arr):
+    arr.sort() # O(NlogN)
+    l = 0
+    r = len(arr)-1
+    result = []
+    while(l<r):
+        if arr[l]+arr[r]==0:
+            result.append([arr[l], arr[r]])
+            l+=1
+        elif arr[l]+arr[r] < 0:
+            l+=1
+        else:
+            r-=1
+        
+    return result
+
+
+# Time complexity - O(N)
+def two_sum_pairs_hashing(arr):
+    hashMap = {}
+    result = []
+    for i in arr:
+        if 0-i not in hashMap:  # O(1)
+            hashMap[i] = 1
+        else:
+            result.append([i, 0-i])
+    return result
+
 arr = [6, 1, 8, 0, 4, -9, -1, -10, -6, -5]
-print(two_sum(arr))
+print(two_sum_pairs_hashing(arr))
+
