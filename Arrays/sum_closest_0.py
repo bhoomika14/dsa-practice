5b3e13c07fc9e1b561ae03c71228d728fc5e2301
bhoomika14 <bhoomikaudaykumar99@gmail.com>
2025-09-23 15:43:33 +0530

    Find sum closest to 0

--- Arrays/sum_closest_0.py
+++ Arrays/sum_closest_0.py
@@ -3,21 +3,16 @@ Given an integer array of N elements.
 
 You need to find the maximum sum of two elements such that sum is closest to zero.
 """
-#Time complexity - O(NlogN), Space complexity - O(1)
-def sum_closest_0(arr):
-    arr.sort()
-    print(arr)
-    l = 0
-    r = len(arr)-1
-    while l<r:
-        minimum = arr[l]+arr[r]
-        if minimum > 0:
-            r-=1
-        elif minimum < 0:
-            l+=1
-        else:
-            return minimum
-    return minimum
+# Brute force - O(N^2)
+def closestToZero(arr):
+    closest = arr[0]+arr[1]
+    for i in range(len(arr)):
+        for j in range(i+1, len(arr)):
+            if abs(arr[i]+arr[j])<abs(closest):
+                closest = arr[i]+arr[j]
+    
+    return closest
+
        
-arr = [5, -3, 7, -8, 10, -1]
-print(sum_closest_0(arr))
+arr = [-32, -12, -6, 13, 29]
+print(closestToZero(arr))
