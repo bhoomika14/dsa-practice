aba3ad9eb844e8c45fd93175d3be3de637b8f507
bhoomika14 <bhoomikaudaykumar99@gmail.com>
2025-09-23 17:29:28 +0530

    Find sum closest to 0

--- Arrays/sum_closest_0.py
+++ Arrays/sum_closest_0.py
@@ -16,3 +16,23 @@ def closestToZero(arr):
        
 arr = [-32, -12, -6, 13, 29]
 print(closestToZero(arr))
+
+# Two pointer approach - O(NLogN)
+def closestSum(arr):
+    arr.sort()
+    l = 0
+    r = len(arr)-1
+    closest = arr[l]+arr[r]
+    while l<r:
+        if abs(arr[l]+arr[r])<abs(closest):
+            closest=arr[l]+arr[r]
+
+        if arr[l]+arr[r]>0:
+            r-=1
+        else:
+            l+=1
+        
+    return closest
+
+arr = [-32, -12, -6, 13, 29]
+print(closestSum(arr))
